819d246ecfafe00cbdd44179b0810c7862e1d3c9
recioz-game <tazadar500@gmail.com>
2025-10-28 22:56:55 +0100

    Actualización: limpieza App y TTS externo

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -14,7 +14,7 @@ const StartMenu: React.FC<StartMenuProps> = ({ onStartGame }) => {
             height: '100vh',
                   backgroundColor: '#0F172A',
       color: '#E5E7EB',
-      backgroundImage: `url(/assets/cover.png)`,
+      backgroundImage: `url(assets/cover.png)`,
             backgroundSize: 'cover',
       backgroundPosition: 'center',
       position: 'relative'
@@ -22,7 +22,7 @@ const StartMenu: React.FC<StartMenuProps> = ({ onStartGame }) => {
             <button 
         onClick={onStartGame}
         style={{
-          background: `url(/assets/play-button.png) no-repeat center center`,
+          background: `url(assets/play-button.png) no-repeat center center`,
           backgroundSize: 'contain',
           width: '200px',
           height: '80px',
